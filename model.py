51e292a9d7364738067b0a9dce66313d9d4dbedf
xianmingli <xmli@stanford.edu>
2018-02-20 22:45:57 -0800

    Read in all covariates so far

--- model.py
+++ model.py
@@ -6,10 +6,10 @@ import datetime
 ###################
 # Reading in data #
 ###################
-# df = pd.read_csv('SBA_Loan_data_.csv',)
+df = pd.read_csv('SBA_Loan_data_.csv',)
 
 '''
-#approval_dates = df.iloc[:]['ApprovalDate']
+# approval_dates = df.iloc[:]['ApprovalDate']
 # months = [datetime.datetime.strptime(x, '%m/%d/%y').date() for x in approval_dates]
 # print(months)
 
@@ -25,49 +25,64 @@ print(test)
 Federal Funds Rates
 '''
 fed_funds_df = pd.read_csv('./covariates_raw/FEDFUNDS.csv')
-print(fed_funds_df);print
+print('fed_funds_df');print(fed_funds_df);print
 
 '''
 Savings Rates
 '''
 savings_rates_df = pd.read_csv('./covariates_raw/SavingsRate.csv')
-print(savings_rates_df);print
+print('savings_rates_df'); print(savings_rates_df);print
 
 '''
 US Dollar Index
 '''
 us_dollar_index_df = pd.read_csv('./covariates_raw/us-dollar-index-historical-chart.csv')
-print(us_dollar_index_df);print
+print('us_dollar_index_df'); print(us_dollar_index_df);print
 
 '''
 Housing Prices
 '''
 housing_prices_df = pd.read_csv('./covariates_raw/Zip_MedianValuePerSqft_AllHomes.csv')
-print(housing_prices_df);print
+print('housing_prices_df'); print(housing_prices_df);print
 
 '''
 US Import Price Index
 '''
 us_import_price_index_df = pd.read_csv('./covariates_raw/US_Import_Price_Index.csv')
-print(us_import_price_index_df);print
+print('us_import_price_index_df'); print(us_import_price_index_df);print
 
 '''
 US Uncertainty Index
 '''
 us_uncertainty_index_df = pd.read_csv('./covariates_raw/US_Uncertainty_Index.csv')
-print(us_uncertainty_index_df);print
+print('us_uncertainty_index_df'); print(us_uncertainty_index_df);print
 
 '''
+Small Business Confidence
 '''
+sm_business_conf_df = pd.read_csv('./covariates_raw/Small_business_confidence.csv')
+print('sm_business_conf_df'); print(sm_business_conf_df);print
 
 '''
-County Unemployment Rate 
-Crisis Indicator Variables 
-National Interest Rate 
-Employment Diffusion 
-Economic Policy Uncertainty Index 
-Political State 
-Small Business Optimism Index 
-National Savings Rate 
-Zillow Housing Aspirations 
+Crisis Indicator
 '''
+crisis_indicator_df = pd.read_csv('./covariates_raw/crisisindicator.csv')
+print('crisis_indicator_df'); print(crisis_indicator_df);print
+
+'''
+National Unemployement Rate
+'''
+natl_unemploy_rate_df = pd.read_csv('./covariates_raw/NationalUnemploymentRateAnnual.csv')
+print('natl_unemploy_rate_df'); print(natl_unemploy_rate_df);print
+
+'''
+US Household Disposable Income
+'''
+us_household_disp_income_df = pd.read_csv('./covariates_raw/US_Household_Disposable_Income.csv')
+print('us_household_disp_income_df'); print(us_household_disp_income_df);print
+
+'''
+US Household Savings
+'''
+us_household_savings_dr = pd.read_csv('./covariates_raw/US_Household_Savings.csv')
+print('us_household_savings_dr'); print(us_household_savings_dr);print
