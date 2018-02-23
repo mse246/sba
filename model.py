0fd65300f2d0f782a99c24875763f786857c6849
xianmingli <xmli@stanford.edu>
2018-02-23 01:23:31 -0800

    finished data covariates

--- model.py
+++ model.py
@@ -7,8 +7,10 @@ import datetime
 # Reading in data #
 ###################
 df = pd.read_csv('SBA_Loan_data_.csv',)
-approval_dates = df.iloc[:]['ApprovalDate']
-print(approval_dates)
+# approval_dates = df.iloc[:]['ApprovalDate']
+# print(approval_dates)
+
+print(df)
 '''
 # approval_dates = df.iloc[:]['ApprovalDate']
 # months = [datetime.datetime.strptime(x, '%m/%d/%y').date() for x in approval_dates]
@@ -25,68 +27,108 @@ print(test)
 '''
 Federal Funds Rates
 '''
-fed_funds_df = pd.read_csv('./covariates_raw/FEDFUNDS.csv')
-print('fed_funds_df');print(fed_funds_df);print
+fed_funds_series = pd.read_csv('./covariates_raw/FEDFUNDS.csv')
 
-for entry in df.iterrows():
-    print(entry['ApprovalDate'])
+for index, row in fed_funds_series.iterrows():
+    if index <= 288:
+        col_name = "FedFundRate_month_%s" % (index)
+        df[col_name] = row['FEDFUNDS']
 
-# '''
-# Savings Rates
-# '''
-# savings_rates_df = pd.read_csv('./covariates_raw/SavingsRate.csv')
-# print('savings_rates_df'); print(savings_rates_df);print
+'''
+Savings Rates
+'''
+savings_rates_series = pd.read_csv('./covariates_raw/SavingsRate.csv')
+for index, row in savings_rates_series.iterrows():
+    if index <= 288:
+        col_name = "SavingsRate_month_%s" % (index)
+        df[col_name] = row['PSAVERT']
 
-# '''
-# US Dollar Index
-# '''
-# us_dollar_index_df = pd.read_csv('./covariates_raw/us-dollar-index-historical-chart.csv')
-# print('us_dollar_index_df'); print(us_dollar_index_df);print
+'''
+US Dollar Index
+'''
+us_dollar_index_series = pd.read_csv('./covariates_raw/us-dollar-index-historical-chart.csv')
+for index, row in us_dollar_index_series.iterrows():
+    if index <= 288:
+        col_name = "USDIndex_month_%s" % (index)
+        df[col_name] = row[' value']
 
 # '''
 # Housing Prices
 # '''
-# housing_prices_df = pd.read_csv('./covariates_raw/Zip_MedianValuePerSqft_AllHomes.csv')
-# print('housing_prices_df'); print(housing_prices_df);print
+# # housing_prices_series = pd.read_csv('./covariates_raw/Zip_MedianValuePerSqft_AllHomes.csv')
+# # for index, row in housing_prices_series.iterrows():
+# #     if index <= 288:
+# #         col_name = "USDIndex_month_%s" % (index)
+# #         df[col_name] = row[' value']
+# # print df
 
-# '''
-# US Import Price Index
-# '''
-# us_import_price_index_df = pd.read_csv('./covariates_raw/US_Import_Price_Index.csv')
-# print('us_import_price_index_df'); print(us_import_price_index_df);print
+'''
+US Import Price Index
+'''
+us_import_price_index_series = pd.read_csv('./covariates_raw/US_Import_Price_Index.csv')
+for index, row in us_import_price_index_series.iterrows():
+    if index <= 288:
+        col_name = "USImportPriceIndex_month_%s" % (index)
+        df[col_name] = row['US Import Price Indexes for Selected Categories of Goods (Monthly)']
 
-# '''
-# US Uncertainty Index
-# '''
-# us_uncertainty_index_df = pd.read_csv('./covariates_raw/US_Uncertainty_Index.csv')
-# print('us_uncertainty_index_df'); print(us_uncertainty_index_df);print
+'''
+US Uncertainty Index
+'''
+us_uncertainty_index_series = pd.read_csv('./covariates_raw/US_Uncertainty_Index.csv')
+for index, row in us_uncertainty_index_series.iterrows():
+    if index <= 288:
+        col_name = "USUncertaintyIndexBaseline_month_%s" % (index)
+        df[col_name] = row['US Economic Policy Uncertainty Index (Monthly - Baseline)']
 
-# '''
-# Small Business Confidence
-# '''
-# sm_business_conf_df = pd.read_csv('./covariates_raw/Small_business_confidence.csv')
-# print('sm_business_conf_df'); print(sm_business_conf_df);print
+for index, row in us_uncertainty_index_series.iterrows():
+    if index <= 288:
+        col_name = "USUncertaintyIndexNews_month_%s" % (index)
+        df[col_name] = row['US Economic Policy Uncertainty Index (Monthly - News Based)']
 
-# '''
-# Crisis Indicator
-# '''
-# crisis_indicator_df = pd.read_csv('./covariates_raw/crisisindicator.csv')
-# print('crisis_indicator_df'); print(crisis_indicator_df);print
+'''
+Small Business Confidence
+'''
+sm_business_conf_series = pd.read_csv('./covariates_raw/Small_business_confidence.csv')
+for index, row in sm_business_conf_series.iterrows():
+    if index <= 288:
+        col_name = "SmallBusinessConfidence_month_%s" % (index)
+        df[col_name] = row['Small Business Optimism']
 
-# '''
-# National Unemployement Rate
-# '''
-# natl_unemploy_rate_df = pd.read_csv('./covariates_raw/NationalUnemploymentRateAnnual.csv')
-# print('natl_unemploy_rate_df'); print(natl_unemploy_rate_df);print
+'''
+Crisis Indicator
+'''
+crisis_indicator_series = pd.read_csv('./covariates_raw/crisisindicator.csv')
+for index, row in crisis_indicator_series.iterrows():
+    if index <= 288:
+        col_name = "CrisisIndicator_month_%s" % (index)
+        df[col_name] = row[' value']
 
-# '''
-# US Household Disposable Income
-# '''
-# us_household_disp_income_df = pd.read_csv('./covariates_raw/US_Household_Disposable_Income.csv')
-# print('us_household_disp_income_df'); print(us_household_disp_income_df);print
+'''
+National Unemployement Rate
+'''
+natl_unemploy_rate_series = pd.read_csv('./covariates_raw/NationalUnemploymentRateAnnual.csv')
+for index, row in natl_unemploy_rate_series.iterrows():
+    for i in range(0, 12):
+        if (index*12)+i <= 288:
+            col_name = "UnemploymentRate_month_%s" % ((index*12)+i)
+            df[col_name] = row['Unemployment rate']
 
-# '''
-# US Household Savings
-# '''
-# us_household_savings_dr = pd.read_csv('./covariates_raw/US_Household_Savings.csv')
-# print('us_household_savings_dr'); print(us_household_savings_dr);print
+'''
+US Household Disposable Income
+'''
+us_household_disp_income_series = pd.read_csv('./covariates_raw/US_Household_Disposable_Income.csv')
+for index, row in us_household_disp_income_series.iterrows():
+    if index <= 288:
+        col_name = "HouseholdDisposableIncome_month_%s" % (index)
+        df[col_name] = row['Household Disposable Income (% Growth Rate)']
+
+'''
+US Household Savings
+'''
+us_household_savings_series = pd.read_csv('./covariates_raw/US_Household_Savings.csv')
+for index, row in us_household_savings_series.iterrows():
+    if index <= 288:
+        col_name = "HouseholdSavings_month_%s" % (index)
+        df[col_name] = row['Household Savings (% of Household Disposable Income)']
+
+df.to_csv('SBA_Loan_data_with_covariates_.csv', encoding='utf-8', index=False)
